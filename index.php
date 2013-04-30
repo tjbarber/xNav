29a91b3b2bf159cb022fe78d8e44dcc99bb87fe1
T.J. Barber <me@tjbarber.org>
2013-04-30 14:11:28 -0500

    Added the original version to repo. Includes an example with the basic styles, the JavaScript, the HTML, and an image.
    Used SCSS for the styling and object-oriented JavaScript to build the
    navigation system.

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>My site</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<ul id="menu">
+			<li><a href="#home">Home</a></li>
+			<li><a href="#contact">Contact</a></li>
+			<li><a href="#about">About</a></li>
+			<li><a href="#play">Play</a></li>
+		</ul>
+			<div id="indicator"></div>
+		<div id="content-pages">
+				
+				<div class="content" id="home">
+					<p>Welcome to my home page.</p>
+				</div>
+				
+				<div class="content" id="contact">
+					<p>You can contact me at 225-244-4074.</p>
+				</div>
+				
+				<div class="content" id="about">
+					<p>I am a Christian web developer.</p>
+				</div>
+				
+				<div class="content" id="play">
+					<p>I like to play Halo: Combat Evolved.</p>
+				</div>
+				
+		</div>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> 
+		<script type="text/javascript" src="js/xnav.js"></script>
+	</body>
+</html>
---
+++ js/xnav.js
@@ -0,0 +1,83 @@
+(function() {
+	
+	$('html').addClass('js');
+	
+	var currentID,
+		currentPosition,
+		currentWidth,
+		currentLink,
+
+		xNav = {
+		
+		config: {
+			effect: 'toggle',
+			speed:undefined,
+			menuItems: undefined,
+			contentContainer: undefined,
+			content: undefined,
+			navHelper: false
+		},
+		
+		init: function(config) {
+			var cf = xNav.config;
+			
+			$.extend(this.config, config);
+		
+			xNav.setData(xNav.config.menuItems.eq(0));
+			cf.contentContainer.show();
+			cf.content.eq(0).show();
+			xNav.setHelper();
+			
+			(this.config.menuItems).on('click', 'a', this.checkStatus); 
+		},
+		
+		setData: function(a) {
+			currentLink = a,
+			currentPosition = currentLink.offset(),
+			currentWidth = currentLink.width();
+		},
+		
+		checkStatus: function(e) {
+			e.preventDefault();
+			xNav.setData($(this));
+			
+			if ( currentID === undefined || currentID !==  currentLink.attr('href')) {
+				xNav.newContentRequest();
+			}; 
+		},
+		
+		newContentRequest: function() {
+			var cf = xNav.config,
+				speed = cf.speed,
+				effect = cf.effect;
+				
+			currentID = currentLink.attr('href');
+			
+			cf.contentContainer.show();
+			cf.content
+				.siblings()
+				.hide();
+			
+			cf.contentContainer
+				.children('div' + currentID)[effect](speed);
+			
+			xNav.setHelper();
+		},
+		
+		setHelper: function() {
+			if (xNav.config.navHelper === true ) $('#indicator')
+				.css('left', currentPosition.left + (currentWidth / 2.5))
+				.show();
+		}
+		
+	};
+	
+	xNav.init({
+		effect: 'fadeToggle',
+		menuItems:$('#menu li'),
+		contentContainer: $('#content-pages'),
+		content: $('#content-pages div'),
+		navHelper: true
+	});
+	
+})();
